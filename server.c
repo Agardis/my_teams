57f9401d841ee24b579fd40d2bb9c4b82fcede77
Théo <theo.klein@edu.esiee-it.fr>
2024-02-15 17:11:58 +0100

    multiple client handled

--- server.c
+++ server.c
@@ -1,4 +1,6 @@
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,68 +9,138 @@
 #include <string.h>
 #include <errno.h>
 #define PORT 56005
+#define MSG_SIZE 100
 
 
-
-void handle_messages(int client_socket)
+int create_server_socket()
 {
-	char buffer[80];
+	struct sockaddr_in server_address;
+	int sockfd;
+	
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	server_address.sin_port = htons(PORT);
 
-	while(1)
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd == -1)
 	{
-		read(client_socket, buffer, sizeof(buffer));
+		printf("socket creation failed\n");
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
 
-		printf("client: %s", buffer);
-	}	
+	if(bind(sockfd, (struct sockaddr*)&server_address, sizeof(server_address)) != 0)
+	{
+		printf("bind failed\n");
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
+	printf("Server open on port: %d\n", PORT);
+
+	if(listen(sockfd, 10) != 0)
+	{
+		printf("listen failed\n");
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
+	printf("Server ready !\n");
+
+	return sockfd;
 }
 
+void accept_new_connection(int server_socket, fd_set *all_sockets, int *max_fd)
+{
+	int new_client = accept(server_socket, NULL, NULL);
+	if(new_client == -1)
+	{
+		printf("Accept failed\n");
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
+	printf("Client accepted\n");
+	FD_SET(new_client, all_sockets);
+	if(new_client > *max_fd)
+		*max_fd = new_client;
 
+	char *buffer = "You are connected to the server";
+	if(send(new_client, buffer, sizeof(buffer), 0) == -1)
+	{
+		printf("send failed to client %i\n", new_client);
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
+}
+
+void read_new_message(int client_socket, fd_set *all_sockets)
+{
+	char received_msg[MSG_SIZE];
+
+	int status = recv(client_socket, received_msg, MSG_SIZE, 0);
+	if(status == 0)
+	{
+		printf("Client %i disconnected\n", client_socket);
+		FD_CLR(client_socket, all_sockets);
+	}
+	else if(status == -1)
+	{
+		printf("recv failed\n");
+		printf("%s\n",strerrorname_np(errno));
+		exit(-1);
+	}
+	else
+		printf("[Client %d]: %s", client_socket, received_msg);
+}
 
 int main(int argc, char const *argv[])
 {
-	struct sockaddr_in server_address, client_address;
+	int server_socket = create_server_socket();
+	
+	fd_set all_sockets;
+	fd_set read_sockets;
+	int max_fd;
+	struct timeval timer;
+	int readable_fd_nbr;
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_address.sin_port = htons(PORT);
+	FD_ZERO(&all_sockets);
+	FD_ZERO(&read_sockets);
+	FD_SET(server_socket, &all_sockets);
 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1)
+	max_fd = server_socket;
+
+	while(1)
 	{
-		printf("socket creation failed \n");
-		exit(0);
+		read_sockets = all_sockets;
+
+		timer.tv_sec = 1;
+		timer.tv_usec = 0;
+
+		readable_fd_nbr = select(max_fd + 1, &read_sockets, NULL, NULL, &timer);
+		
+		if(readable_fd_nbr == -1)
+		{
+			printf("Select failed \n");
+			printf("%s\n",strerrorname_np(errno));
+			exit(-1);
+		}
+		else if(readable_fd_nbr == 0)
+		{
+			continue;
+		}
+		for (int i = 0; i <= max_fd; i++)
+		{
+			if(FD_ISSET(i, &read_sockets) == 0)
+				continue;
+			if(i == server_socket)
+			{
+				accept_new_connection(server_socket, &all_sockets, &max_fd);
+			}
+			else
+			{
+				read_new_message(i, &all_sockets);
+			}
+		}
 	}
-	printf("socket created ! \n");
 
-	if((bind(sockfd, (struct sockaddr*)&server_address, sizeof(server_address))) != 0)
-	{
-		printf("bind failed \n");
-		printf("%s\n",strerrorname_np(errno));
-		exit(0);
-	}
-	printf("socket binded to port : %d \n", PORT);
-
-	if(listen(sockfd, 3) != 0)
-	{
-		printf("listen failed \n");
-		exit(0);
-	}
-	printf("server is listening !\n");
-
-	int len = sizeof(client_address);
-
-	int client_socket = accept(sockfd, (struct sockaddr*)&client_address, &len);
-
-	if( client_socket < 0)
-	{
-		printf("connection rejected \n");
-		printf("%s\n",strerror(errno));
-		exit(0);
-	}
-
-	handle_messages(client_socket);
-
-	close(sockfd);
 
 	return 0;
 }
